be2b5edfde759d46177e526995f3e30a01bb6322
jonatanprim <49272956+jonatanprim@users.noreply.github.com>
2019-04-04 07:32:14 -0700

    Update index.php

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "<tugas1azure.database.windows.net>";
+    $user = "<jonatan>";
+    $pass = "<melisa_177>";
+    $db = "<Tugas1Azure>";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
